1e1b317550251b3ddd5583e87327ba6a2a44ae53
Caio-Albuquerque22 <caiocvla@gmail.com>
2026-07-23 21:15:47 +0000

    passando arquivos para raiz do projeto

--- api/index.js
+++ api/index.js
@@ -1,13 +1,11 @@
 const addonInterface = require("../addon");
 
 module.exports = (req, res) => {
-    // Redireciona a raiz para o manifest.json
     if (req.url === "/" || req.url === "") {
         res.writeHead(307, { Location: "/manifest.json" });
         res.end();
         return;
     }
 
-    // Passa a requisição para o SDK do Stremio
     addonInterface.getRouter()(req, res);
 };
