d5b11ba2f0ee08aa69f01edf0e58569f235e8fa2
Caio-Albuquerque22 <caiocvla@gmail.com>
2026-07-23 21:57:47 +0000

    atualizando rotas com express

--- api/index.js
+++ api/index.js
@@ -1,14 +1,11 @@
+const express = require("express");
 const { getRouter } = require("stremio-addon-sdk");
 const addonInterface = require("../addon");
 
-// Importamos o roteador oficial do SDK do Stremio
-const router = getRouter(addonInterface);
+const app = express();
 
-module.exports = (req, res) => {
-    // Passamos a requisição da Vercel para o roteador do Stremio
-    // O terceiro parâmetro resolve o erro 500 garantindo que a Vercel não quebre
-    router(req, res, () => {
-        res.statusCode = 404;
-        res.end();
-    });
-};
+// O Express repassa automaticamente a rota raiz "/" (a tela bonita) 
+// e a "/manifest.json" para o SDK do Stremio
+app.use("/", getRouter(addonInterface));
+
+module.exports = app;
